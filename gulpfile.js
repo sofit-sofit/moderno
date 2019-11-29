812b1adc61fbb8ddeb0484c96a63da6f88608a26
sofit-sofit <alesamokrickaa@gmail.com>
2019-11-29 12:45:26 +0200

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@
 
 
 gulp.task('sass', function(){
-   return gulp.src('app/scss/style.scss')
+   return gulp.src('app/scss/**/*.scss')
             .pipe(sass({outputStyle: 'compressed'}))
             .pipe(rename({suffix : ".min"}))
             .pipe(autoprefixer({
@@ -67,7 +67,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass') )
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass') )
     gulp.watch('app/*.html', gulp.parallel('html') )
     gulp.watch('app/js/*.js', gulp.parallel('js') )
 
